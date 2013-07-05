8a9b948e03dcab64f7e812eeae481aa36fe047f5
Brad Campbell <bradjc5@gmail.com>
2013-07-05 15:45:27 -0400

    Cleaned up receiver_udp, added receiver_tcp
    Removed the old receiver_tcp.c in favor of a python implementation.
    
    Changed to tup :)

---
+++ receiver/transmitter/transmitter_tcp.py
@@ -0,0 +1,19 @@
+import socket
+import sys
+import time
+
+HOST, PORT = "inductor.eecs.umich.edu", 4002
+data = "a"
+
+# Create a socket (SOCK_STREAM means a TCP socket)
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+try:
+	# Connect to server and send data
+	sock.connect((HOST, PORT))
+	for i in range(0,10):
+		sock.sendall("\x42")
+		time.sleep(1)
+
+finally:
+	sock.close()
