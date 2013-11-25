c350a60b68882b795dcde5b596d71e4f0b16d9ea
Brad Campbell <bradjc5@gmail.com>
2013-11-25 13:09:58 -0500

    added http receiver to run script

--- receiver/run_receiver.sh
+++ receiver/run_receiver.sh
@@ -3,3 +3,4 @@
 screen -S gatd-receiver-udp -m -d ./receiver-udp
 screen -S gatd-receiver-tcp -m -d python receiver_tcp.py
 screen -S gatd-receiver-udp-ipv4 -m -d python receiver_udp_ipv6_over_ipv4.py
+screen -S gatd-receiver-http-post -m -d python receiver_http_post.py
