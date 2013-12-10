1e819a710081854a4c8c29cfdd1b5a2cce4a267e
Brad Campbell <bradjc5@gmail.com>
2013-12-10 17:57:04 -0500

    updated gatd http post to use do_POST
    The http receiver should be much more stable now.
    Also cleaned it up a bit.

--- receiver/receiver_http_post.py
+++ receiver/receiver_http_post.py
@@ -1,54 +1,30 @@
-#!/usr/bin/python
-
-
 from BaseHTTPServer import BaseHTTPRequestHandler,HTTPServer
+import IPy
 import json
 import pika
 import struct
-import IPy
 import time
 import urlparse
 
-PORT_NUMBER = 8081
+LISTEN_HOST = ''
+LISTEN_PORT = 8081
 
-HOSTNAME = "localhost"
 RECEIVE_EXCHANGE = "receive_exchange"
 
-MAX_INCOMING_LENGTH = 4096
 PKT_TYPE_HTTP_POST = 3
 
-class myHandler(BaseHTTPRequestHandler):
-	requestline = ''
-	request_version = 'HTTP/1.0'
+class gatdPostHandler (BaseHTTPRequestHandler):
 
-	def handle(self):
+	def do_POST(self):
 		now = int(time.time()*1000)
 
 		amqp_conn = pika.BlockingConnection(pika.ConnectionParameters(
 			host=HOSTNAME))
 		amqp_chan = amqp_conn.channel();
 
-		header_str = ''
-		while True:
-			byte = self.rfile.read(1)
-			header_str += byte
-			if len(header_str) > 3 and header_str[-4:] == '\r\n\r\n':
-				break
-		headers = header_str.split('\n')
-		length = 0
-		for header in headers:
-			val = header.split(': ')
-			if val[0] == 'Content-Length':
-				length = int(val[1])
-			if header[0:4] == 'POST':
-				posts = header.split(" ")
-				post_str = posts[1]
-
-		if length == 0:
-			return
-
-		poststr = self.rfile.read(length)
-		data = urlparse.parse_qs(poststr)
+		content_len = int(self.headers.getheader('content-length'))
+		post_body   = self.rfile.read(content_len)
+		data        = urlparse.parse_qs(post_body)
 
 		# Get the IPv6 address in integer form
 		try:
@@ -66,7 +42,7 @@ class myHandler(BaseHTTPRequestHandler):
 			port,
 			now)
 
-		amqp_pkt += post_str + json.dumps(data)
+		amqp_pkt += self.path + json.dumps(data)
 
 		print(amqp_pkt)
 
@@ -85,9 +61,9 @@ class myHandler(BaseHTTPRequestHandler):
 
 try:
 	# Create a web server and define the handler to manage the incoming request
-	server = HTTPServer(('', PORT_NUMBER), myHandler)
+	server = HTTPServer((LISTEN_HOST, LISTEN_PORT), gatdPostHandler)
 
-	# Wait forever for incoming http requests
+	# Wait forever for incoming HTTP requests
 	server.serve_forever()
 
 except KeyboardInterrupt:
