5a76ca749e86acc791ca17ee1ce05a80b9b22831
Brad Campbell <bradjc5@gmail.com>
2013-12-21 02:36:54 -0500

    added python socket to run streamer

--- streamer/run_streamer.sh
+++ streamer/run_streamer.sh
@@ -2,4 +2,5 @@
 
 screen -L -S gatd-streamer-socketio -d -m forever streamer_socketio.js | tee -a streamer-socketio.log
 screen -L -S gatd-streamer-tcp -d -m python streamer_tcp.py | tee -a streamer-tcp.log
+screen -L -S gatd-streamer-socketiopy -d -m python streamer_socketio.py | tee -a streamer-socketiopy.log
 
