537685b03604ac4b1b105b25beae5fce4142b69c
Brad Campbell <bradjc5@gmail.com>
2014-02-01 15:26:46 -0800

    Updated receivers
    Now generate config data from gatd.config and use a header file for
    that. Added that generation to the tupfile.

--- receiver/receiver_udp.h
+++ receiver/receiver_udp.h
@@ -7,18 +7,6 @@
 #include <amqp_framing.h>
 #include "utils.h"
 
-
-#define UDP_RECEIVE_PORT 4001
-#define PKT_TYPE_UDP 0
-
-extern const char           amqp_hostname[];
-extern const int            amqp_port;
-extern const char           amqp_username[];
-extern const char           amqp_password[];
-extern const amqp_bytes_t   amqp_exchange;
-extern const amqp_bytes_t   amqp_routing_key;
-extern const amqp_channel_t amqp_channel;
-
 extern const int MESSAGE_ITEM_BASE_SIZE;
 
 
--- receiver/run_receiver.sh
+++ receiver/run_receiver.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
 screen -S gatd-receiver-udp -m -d ./receiver-udp
-screen -S gatd-receiver-tcp -m -d python receiver_tcp.py
-screen -S gatd-receiver-udp-ipv4 -m -d python receiver_udp_ipv6_over_ipv4.py
-screen -S gatd-receiver-http-post -m -d python receiver_http_post.py
+screen -S gatd-receiver-tcp -m -d python2 receiver_tcp.py
+screen -S gatd-receiver-http-post -m -d python2 receiver_http_post.py
