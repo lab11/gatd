a8273cc57e1a05aadec17cab936c1ed23f0e97c1
Brad Campbell <bradjc5@gmail.com>
2014-02-02 15:38:33 -0500

    set python2

--- streamer/run_streamer.sh
+++ streamer/run_streamer.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 screen -L -S gatd-streamer-socketio -d -m forever streamer_socketio.js | tee -a streamer-socketio.log
-screen -L -S gatd-streamer-tcp -d -m python streamer_tcp.py | tee -a streamer-tcp.log
-screen -L -S gatd-streamer-socketiopy -d -m python streamer_socketio.py | tee -a streamer-socketiopy.log
+screen -L -S gatd-streamer-tcp -d -m python2 streamer_tcp.py | tee -a streamer-tcp.log
+screen -L -S gatd-streamer-socketiopy -d -m python2 streamer_socketio.py | tee -a streamer-socketiopy.log
 
