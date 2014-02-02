588a59fa73c6d3bfea880c6f24e29d3a62929560
Brad Campbell <bradjc5@gmail.com>
2014-02-02 15:38:42 -0500

    Cleaned up mongo file for python socketio streamer
    seems to be working

--- streamer/MongoInterface.py
+++ streamer/MongoInterface.py
@@ -14,9 +14,8 @@ class MongoInterface:
 			self.mongo_conn = pymongo.MongoClient(host=gatdConfig.mongo.HOST,
 			                                      port=gatdConfig.mongo.PORT)
 			self.mongo_db = self.mongo_conn[gatdConfig.mongo.DATABASE]
-			if gatdConfig.mongo.USERNAME:
-				self.mongo_db.authenticate(gatdConfig.mongo.USERNAME,
-				                           gatdConfig.mongo.PASSWORD)
+			self.mongo_db.authenticate(gatdConfig.mongo.USERNAME,
+			                           gatdConfig.mongo.PASSWORD)
 			self.stop = False
 
 		except pymongo.errors.ConnectionFailure:
@@ -25,22 +24,15 @@ class MongoInterface:
 
 	def get (self, query):
 
-		# Trial period:
-		# Let's just use this capped collection for everything cause why not
-		# try.
-		# time is the milliseconds in the past from now to start getting
-		# data from.
+		# Use the capped collection to implement streaming starting at a time
+		# in the past. If the time key is present it is used as the minimum
+		# timestamp.
 		now = int(round(time.time() * 1000))
 		if 'time' in query:
 			start = now - query['time']
 		else:
 			start = now
 		query['time'] = {'$gt': start}
-
-		# We are only streaming out of the tailable capped collection
-		# Make sure we only get packets from now onward
-	#	now = int(round(time.time() * 1000))
-	#	query['time'] = {'$gt': now}
 
 		cursor = self.mongo_db[gatdConfig.mongo.COL_FORMATTED_CAP].find(query,
 			tailable=True,
