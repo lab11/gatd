ed19c272ce5527d4a42cd5afb79e8d437009bb61
Brad Campbell <bradjc5@gmail.com>
2014-02-06 18:16:52 -0500

    python2 for receivers

--- receiver/receiver_tcp.py
+++ receiver/receiver_tcp.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python2
+
 import IPy
 import json
 import os
--- receiver/run_receiver.sh
+++ receiver/run_receiver.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 screen -S gatd-receiver-udp -m -d ./receiver-udp
-screen -S gatd-receiver-tcp -m -d python2 receiver_tcp.py
-screen -S gatd-receiver-http-post -m -d python2 receiver_http_post.py
+screen -S gatd-receiver-tcp -m -d ./receiver_tcp.py
+screen -S gatd-receiver-http-post -m -d ./receiver_http_post.py
