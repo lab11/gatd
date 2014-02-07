76c9a4f66bf02586dfc1ee7c40eb670f1acae39e
Brad Campbell <bradjc5@gmail.com>
2014-02-07 12:47:36 -0500

    added stub parser for m3 testing logging

---
+++ formatter/parsers/m3TestingLoggerParser.py
@@ -0,0 +1,25 @@
+import binascii
+import IPy
+import json
+import struct
+import parser
+
+class m3TestingLoggerParser (parser.parser):
+
+	# Parameters for this profile
+	name = 'M3 Testers Logging'
+	access = 'public'
+
+	def __init__ (self):
+		pass
+
+	def parse (self, data, meta, extra, settings):
+		ret = {}
+
+		ret['address'] = str(meta['addr'])
+		ret['port']    = meta['port']
+		ret['time']    = meta['time']
+		ret['public']  = settings['public']
+
+		return ret
+
