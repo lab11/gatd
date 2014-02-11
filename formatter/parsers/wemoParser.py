796992d593ca69bc5268e4c2f2199a683252c226
Brad Campbell <bradjc5@gmail.com>
2014-02-11 15:21:25 -0500

    wemo queryer

---
+++ formatter/parsers/wemoParser.py
@@ -0,0 +1,23 @@
+import binascii
+import IPy
+import json
+import struct
+import parser
+
+class wemoParser (parser.parser):
+
+	# Parameters for this profile
+	name = 'WeMo Parser'
+
+	def __init__ (self):
+		pass
+
+	def parse (self, data, meta, extra, settings):
+		ret = json.loads(data[10:])
+
+		ret['port']    = meta['port']
+		ret['address'] = str(meta['addr'])
+		ret['time']    = meta['time']
+		ret['public']  = settings['public']
+
+		return ret
