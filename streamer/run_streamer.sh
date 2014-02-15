a8b9b4f91df7975bbebcffcbcf900502299529c8
Brad Campbell <bradjc5@gmail.com>
2014-02-15 16:33:36 -0500

    added new streamer to run script

--- streamer/run_streamer.sh
+++ streamer/run_streamer.sh
@@ -3,3 +3,4 @@
 screen -L -S gatd-streamer-socketio -d -m forever streamer_socketio.js | tee -a streamer-socketio.log
 screen -L -S gatd-streamer-tcp -d -m ./streamer_tcp.py | tee -a streamer-tcp.log
 screen -L -S gatd-streamer-socketiopy -d -m ./streamer_socketio.py | tee -a streamer-socketiopy.log
+screen -L -S gatd-streamer-socketiopyhist -d -m ./streamer_socketio_historical.py | tee -a streamer-socketiopyhist.log
