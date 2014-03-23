07ffb35cc2eefd8db205df5ea7650eba4e41c3f5
Brad Campbell <bradjc5@gmail.com>
2014-03-23 18:34:20 -0400

    updated opo parser

--- formatter/parsers/opoParser.py
+++ formatter/parsers/opoParser.py
@@ -10,22 +10,17 @@ class opoParser (parser.parser):
 
 	def parse (self, data, meta, extra, settings):
 		ret = {}
-		print(len(data))
-		print(binascii.hexlify(data))
+
 		# Opo-Specific
-		s = struct.unpack('!10s H H 5B B H H H H H H', data)
-		gatd_profile_id                  = s[0]
-		ret['tx_id']                     = s[1]
-		ret['seq']                       = s[2]
-		ret['full_time']                 = s[3:8]
-		ret['buffer_index']              = s[8]
-		ret['last_tx_id']                = s[9]
-		ret['t_rf']                      = s[10]
-		ret['t_ultrasonic_wake']         = s[11]
-		ret['t_ultrasonic_wake_falling'] = s[12]
-		ret['t_ultrasonic']              = s[13]
-		ret['t_ultrasonic_falling']      = s[14]
-		ret['range']                     = (float(ret['t_ultrasonic']) - float(ret['t_rf']))/32000.0 * 340.29 - .12
+		s = struct.unpack('!10s H I H H 5B I', data)
+		gatd_profile_id   = s[0]
+		ret['tx_id']      = s[1]
+		ret['seq']        = s[2]
+		ret['last_tx_id'] = s[3]
+		ret['t_rf']       = s[4]
+		ret['full_time']  = s[5:10]
+		ret['last_seq']   = s[11]
+		ret['range']      = (float(ret['t_ultrasonic']) - float(ret['t_rf']))/32000.0 * 340.29 - .12
 
 		print(ret)
 
