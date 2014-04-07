5aaee030d349b5c935921d31e19e93c8911110a3
Brad Campbell <bradjc5@gmail.com>
2014-04-07 00:43:32 -0400

    updated run scripts

--- processor/run_processor.sh
+++ processor/run_processor.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-screen -S gatd-processor -d -m ./processor.py monjolo_freq
-screen -S gatd-processor -d -m ./processor.py monjolo_ground_truth
-screen -S gatd-processor -d -m ./processor.py wattsup_aggregate
+screen -S gatd-processor-monjolo-freq -d -m ./processor.py monjolo_freq
+screen -S gatd-processor-monjolo-gt   -d -m ./processor.py monjolo_ground_truth
+screen -S gatd-processor-wattsup-agg  -d -m ./processor.py wattsup_aggregate
--- streamer/run_streamer.sh
+++ streamer/run_streamer.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-screen -L -S gatd-streamer-socketio -d -m forever streamer_socketio.js | tee -a streamer-socketio.log
-screen -L -S gatd-streamer-tcp -d -m ./streamer_tcp.py | tee -a streamer-tcp.log
-screen -L -S gatd-streamer-socketio-py -d -m ./streamer_socketio.py | tee -a streamer-socketiopy.log
-screen -L -S gatd-streamer-socketio-py-hist -d -m ./streamer_socketio_historical.py | tee -a streamer-socketiopyhist.log
-screen    -S gatd-streamer-socketio-py-replay -d -m ./streamer_socketio_replay.py
+screen -S gatd-streamer-socketio -d -m forever streamer_socketio.js
+screen -S gatd-streamer-tcp -d -m ./streamer_tcp.py
+screen -S gatd-streamer-socketio-py -d -m ./streamer_socketio.py
+screen -S gatd-streamer-socketio-py-hist -d -m ./streamer_socketio_historical.py
+screen -S gatd-streamer-socketio-py-replay -d -m ./streamer_socketio_replay.py
