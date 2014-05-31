8fba617184a55d31724594d53e28a2dc21c10af7
Brad Campbell <bradjc5@gmail.com>
2014-05-30 18:04:18 -0700

    change to underscore in run script

--- receiver/run_receiver.sh
+++ receiver/run_receiver.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-screen -S gatd-receiver-udp -m -d ./receiver-udp.py
+screen -S gatd-receiver-udp -m -d ./receiver_udp.py
 #screen -S gatd-receiver-udp -m -d ./receiver-udp
 screen -S gatd-receiver-tcp -m -d ./receiver_tcp.py
 screen -S gatd-receiver-http-post -m -d ./receiver_http_post.py
