8b14762cb87bae336f7f3bbcf7a9dfb2f36b3eb1
Brad Campbell <bradjc5@gmail.com>
2014-06-24 14:09:50 -0400

    update duplicator to hopefully use less memory

--- formatter/Deduplicator.py
+++ formatter/Deduplicator.py
@@ -1,3 +1,5 @@
+import time
+
 #
 # Returns true if the packet is unique, false if it is a duplicate
 #
@@ -6,11 +8,19 @@
 # a duplicate.
 # In any case the packet is added with its timestamp
 
+TIME_DELAY = 2000
+
 class Deduplicator (object):
 	def __init__ (self):
 
 		# dict of packet -> timestamp
 		self.packet_hashes = {}
+
+		# array of timestamps
+		self.timestamps = []
+
+		# dict of timestamp -> packet
+		self.time_hashes = {}
 
 
 	# Call this function on the incoming data message to check if it is
@@ -26,10 +36,39 @@ class Deduplicator (object):
 		previous_time = self.packet_hashes.setdefault(key, 0)
 
 		if previous_time > 0:
-			if abs(time - previous_time) < 2000:
+			if abs(time - previous_time) < TIME_DELAY:
 				duplicate = True
 
 		# Put this packet in the dict for the next go around
 		self.packet_hashes[key] = time
+
+		# Save the timestamp and backwards lookup
+		self.timestamps.append(time)
+		self.time_hashes[time] = key
+
+		# Get rid of old packets from the dicts to reduce memory usage
+		now = int(time.time()*1000)
+		while True:
+			time_check = self.timestamps[0]
+			if now - time_check > TIME_DELAY:
+				# Deleting this timestamp
+				self.timestamps.remove(0)
+
+				# Get the packet that corresponds to that time
+				pkt = self.time_hashes[time_check]
+
+				# Get the timestamp that that packet came in at
+				saved_time = self.packet_hashes[pkt]
+
+				# Check that a newer duplicate packet didn't come in and
+				# update the timestamp
+				if saved_time == time_check:
+					# Go ahead and delete the packet
+					del self.packet_hashes[pkt]
+					del self.time_hashes[time_check]
+
+			else:
+				break
+
 
 		return duplicate
