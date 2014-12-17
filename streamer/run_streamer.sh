e7b5390a16cbff4d320a5e17b3c4d41b9199389c
bradjc <bradjc@from.eecs.umich.edu>
2014-12-17 02:17:37 -0500

    fix streamer

--- streamer/run_streamer.sh
+++ streamer/run_streamer.sh
@@ -2,6 +2,6 @@
 
 #screen -S gatd-streamer-socketio -d -m forever streamer_socketio.js
 screen -S gatd-streamer-tcp -d -m ./streamer_tcp.py
-screen -S gatd-streamer-socketio-py        -d -m ./streamer_socketio.py new
+screen -S gatd-streamer-socketio-py-base   -d -m ./streamer_socketio.py new
 screen -S gatd-streamer-socketio-py-hist   -d -m ./streamer_socketio.py all
 screen -S gatd-streamer-socketio-py-replay -d -m ./streamer_socketio.py replay
